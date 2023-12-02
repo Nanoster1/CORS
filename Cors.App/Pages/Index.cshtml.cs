886501afa6df88d82a686d46bc962f8129e4c590
Nanoster1 <danil2002.11@mail.ru>
2023-12-02 12:17:01 +0500

    Added App

---
+++ Cors.App/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cors.App.Pages;
+
+public class IndexModel : PageModel
+{
+    public IndexModel()
+    {
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
